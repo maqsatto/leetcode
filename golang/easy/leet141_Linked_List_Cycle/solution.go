178810f3c5b0385573004d470822cea687c86f29
maqsatto <m.syrtbay@outlook.com>
2025-12-27 23:26:07 +0500

    leet141 solved

---
+++ golang/easy/leet141_Linked_List_Cycle/solution.go
@@ -0,0 +1,21 @@
+package leet141
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func hasCycle(head *ListNode) bool {
+	if head == nil {
+		return false
+	}
+	fast, slow := head.Next, head
+	for fast != nil && fast.Next != nil {
+		if fast == slow {
+			return true
+		}
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return false
+}
