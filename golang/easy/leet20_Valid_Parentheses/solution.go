cb1cb1ea350d6bd7776793152afa5f5ef4263d29
maqsatto <m.syrtbay@outlook.com>
2025-12-28 13:04:26 +0500

    leet20 not solved

---
+++ golang/easy/leet20_Valid_Parentheses/solution.go
@@ -0,0 +1,16 @@
+package leet20
+
+import (
+	"strings"
+)
+
+// not solved
+func isValid(s string) bool {
+
+	valid := false
+	str := strings.Split(s, "")
+	for _, v := range str {
+
+	}
+	return valid
+}
--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,9 @@
 package main
 
-import leet2 "github.com/maqsatto/leetcode/golang/medium/leet2_Add_Two_Numbers"
+import (
+	leet21 "github.com/maqsatto/leetcode/golang/easy/leet21_Merge_Two_Sorted_Lists"
+)
 
 func main() {
-	leet2.TestFunc()
+	leet21.TestMergeTwoLists()
 }
